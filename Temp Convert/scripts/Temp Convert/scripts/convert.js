7ab1c7a65848955c3da155ae26b3c29be4171595
Hemlock115 <98435562+Hemlock115@users.noreply.github.com>
2022-01-25 20:17:44 -0500

    Delete Temp Convert/scripts directory

--- Temp Convert/scripts/Temp Convert/scripts/convert.js
+++
@@ -1,6 +0,0 @@
-let f = prompt("Enter Fahrenheit Temperature");
-f = parseInt(f);
-let c = (f-32) * 5/9;
-c = parseInt(c);
-alert("Fahrenheit temperature is " + f 
-    + "\n\nCelsius temperatures is " + c); 
