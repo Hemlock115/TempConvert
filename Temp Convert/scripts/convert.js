5ffd67d4af0394cc1e11dd7fb37b06701b2e171a
Hemlock115 <98435562+Hemlock115@users.noreply.github.com>
2022-01-25 19:38:40 -0500

    First Commit

---
+++ Temp Convert/scripts/convert.js
@@ -0,0 +1,6 @@
+let f = prompt("Enter Fahrenheit Temperature");
+f = parseInt(f);
+let c = (f-32) * 5/9;
+c = parseInt(c);
+alert("Fahrenheit temperature is " + f 
+    + "\n\nCelsius temperatures is " + c); 
